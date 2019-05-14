c11169ec7c8752c23bf4b4e29dc0f42c13944dfb
romeo-07 <50551936+romeo-07@users.noreply.github.com>
2019-05-14 03:39:44 -0700

    Configuration file restructure
    Labelled files PROTECT that have sensitive information; Default has some PII in it; Keys has some $ compromise info in it.  Once downloaded these should be renamed and not be sync back to repository.

---
+++ Spot_Defaults_PROTECT.py
@@ -0,0 +1,11 @@
+# Default values
+xDefault=''
+yDefault=''
+gridDefault=''
+zipDefault=''
+zipcode=zipDefault
+grid=gridDefault
+
+# Google API Path/Key
+serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+key='&key='
