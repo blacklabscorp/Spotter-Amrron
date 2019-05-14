ebfe83583b44f4f89f0c67c258f7066e100e333a
romeo-07 <50551936+romeo-07@users.noreply.github.com>
2019-05-13 19:38:29 -0700

    Sanitized config file

---
+++ Spot_Defaults_COPY.py
@@ -0,0 +1,24 @@
+# Default values
+xDefault=''
+yDefault=''
+dmrIdDefault=
+callsignDefault=''
+gridDefault=''
+zipDefault=''
+callsign=""
+zipcode=zipDefault
+grid=gridDefault
+romeo07=''
+oscarRomeo01=''
+twilioAmrron=''
+
+# Google API Path/Key
+serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+key='&key='
+googleKey=''
+
+# Twilio API Path/Key
+twilioAccountSid = ''
+twilioAuthToken = ''
+
+
