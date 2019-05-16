3ddbaf1d3930016141e666e9f0a31208b4deacaa
romeo-07 <50551936+romeo-07@users.noreply.github.com>
2019-05-15 22:42:35 -0700

    Solar data pull
    -add solar data pull
    -add Env set python file

---
+++ Spot_Solar.py
@@ -0,0 +1,10 @@
+﻿# Solar conditions
+import requests
+from bs4 import BeautifulSoup
+from Spot_Defaults import solarServiceUrl
+from Spot_Keys_PC import solarKey
+
+def solarReports():
+        page = requests.get(solarServiceUrl)
+        soup = BeautifulSoup(page.text,'html.parser')
+        print (soup)
---
+++ Test_Spot_Solar.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+from Spot_Defaults import solarServiceUrl
+from Spot_Keys_PC import solarKey
+
+# Test Main body
+solarReports()
+print (soup)
