101c51edfb70aea199c71591ddc7f930356b7ec2
romeo-07 <50551936+romeo-07@users.noreply.github.com>
2019-06-06 18:53:38 -0700

    Spot_Configs
    JSON output of Environment variables.  Listed variables are required to be part of the Env in order for Spotter to run.

---
+++ Spot_Configs.py
@@ -0,0 +1,70 @@
+import json
+import os
+
+callsignDefault=os.environ["s_callsignDefault"]
+dmrIdDefault=os.environ["s_dmrIdDefault"]
+xDefault=os.environ["s_xDefault"]
+yDefault=os.environ["s_yDefault"]
+gridDefault=os.environ["s_gridDefault"]
+postalcodeDefault=os.environ["s_postalcodeDefault"]
+k2sDefault=os.environ["s_k2sDefault"]
+googleKey=os.environ["s_googleKey"]
+accuWeatherKey=os.environ["s_accuWeatherKey"]
+OpenWeatherKey=os.environ["s_OpenWeatherKey"]
+twilioAccountSid=os.environ["s_twilioAccountSid"]
+twilioAmrronCell=os.environ["s_twilioAmrronCell"]
+twilioAuthToken=os.environ["s_twilioAuthToken"]
+amrron00User=os.environ["s_amrron00User"]
+amrron01User=os.environ["s_amrron01User"]
+#amrron02User=os.environ["s_amrron02User"]
+#amrron03User=os.environ["s_amrron03User"]
+#amrron04User=os.environ["s_amrron04User"]
+#amrron05User=os.environ["s_amrron05User"]
+amrron00Cell=os.environ["s_amrron00Cell"]
+amrron01Cell=os.environ["s_amrron01Cell"]
+#amrron02Cell=os.environ["s_amrron02Cell"]
+#amrron03Cell=os.environ["s_amrron03Cell"]
+#amrron04Cell=os.environ["s_amrron04Cell"]
+#amrron05Cell=os.environ["s_amrron05Cell"]
+amrron00Email=os.environ["s_amrron00Email"]
+amrron01Email=os.environ["s_amrron01Email"]
+#amrron02Email=os.environ["s_amrron02Email"]
+#amrron03Email=os.environ["s_amrron03Email"]
+#amrron04Email=os.environ["s_amrron04Email"]
+#amrron05Email=os.environ["s_amrron05Email"]
+
+Configs={}
+Configs={'callsignDefault':callsignDefault,
+         'dmrIdDefault':dmrIdDefault,
+         'k2sDefault':k2sDefault,
+         'xDefault':xDefault,
+         'yDefault':yDefault,
+         'gridDefault':gridDefault,
+         'postalcodeDefault':postalcodeDefault,
+         'googleKey':googleKey,
+         'accuWeatherKey':accuWeatherKey,
+         'OpenWeatherKey':OpenWeatherKey,
+         'twilioAccountSid':twilioAccountSid,
+         'twilioAmrronCell':twilioAmrronCell,
+         'twilioAuthToken':twilioAuthToken,
+         'amrron00User':amrron00User,
+         'amrron01User':amrron01User,
+         #'amrron02User':amrron02User,
+         #'amrron03User':amrron03User,
+         #'amrron04User':amrron04User,
+         #'amrron05User':amrron05User,
+         'amrron00Cell':amrron00Cell,
+         'amrron01Cell':amrron01Cell,
+         #amrron02Cell':amrron02Cell,
+         #amrron03Cell':amrron03Cell,
+         #amrron04Cell':amrron04Cell,
+         #amrron05Cell':amrron05Cell,
+         'amrron00Email':amrron00Email,
+         'amrron01Email':amrron01Email,
+         #amrron02Email':amrron02Email,
+         #amrron03Email':amrron03Email,
+         #amrron04Email':amrron04Email,
+         #amrron05Email':amrron05Email
+         }
+with open('Configs.json', 'w') as f:
+                json.dump(Configs, f, ensure_ascii=False)
